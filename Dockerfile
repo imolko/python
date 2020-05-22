84e55a4ba4c8db8ea8067ffe26daf9b60e56f1bc
Yohany Flores Suarez <yohanyflores@gmail.com>
2020-05-21 22:09:54 -0500

    Dockerfile Created!

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM python:3
+
+ENV GOSU_VERSION 1.12
+RUN set -x \
+  && curl -sSLo /usr/local/bin/gosu "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$(dpkg --print-architecture)" \
+  && curl -sSLo /usr/local/bin/gosu.asc "https://github.com/tianon/gosu/releases/download/$GOSU_VERSION/gosu-$(dpkg --print-architecture).asc" \
+  && export GNUPGHOME="$(mktemp -d)" \
+  && gpg --keyserver ha.pool.sks-keyservers.net --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4 \
+  && gpg --batch --verify /usr/local/bin/gosu.asc /usr/local/bin/gosu \
+  && rm -r "$GNUPGHOME" /usr/local/bin/gosu.asc \
+  && chmod +x /usr/local/bin/gosu \
+  && gosu nobody true
+
+# Creamos un user dev
+RUN groupadd -r dev --gid=9001 && useradd -r -g dev --uid=9001 dev
+
+# Copiamos los entry point
+COPY entrypoints /entrypoints
---
+++ entrypoints/pants.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+if [ "$(id -u)" = "0" ]; then
+  if [ -n "${XUID}" ] && [ -n "${XGID}" ]; then
+      export USER=dev
+
+      # Cambiamos el id del usuario dev
+      usermod -u ${XUID} dev 2> /dev/null && {
+          groupmod -g ${XGID} dev 2> /dev/null || usermod -a -G ${XGID} dev
+      }
+
+      exec /usr/local/bin/gosu dev "$@"
+  fi
+fi
+
+# Ejecutamos el comando normal
+exec "$@"
+
+# Hay otro metodo para impersonal el usuario creando el usuario al momento de entrar al endpoint
+# ESto era otro metodo para impersonar el usuario especificado poc XUID.
+# Que crea el usuario al vuelo.
+#
+#XGROUP="$(getent group "${XGID}" | cut -d: -f1)"
+#if [ -z "${XUSER}" ]; then
+#    XGROUP="developer"
+#    groupadd -g "${XGID}" "developer"
+#fi
+
+#XUSER="$(getent passwd "${XUID}" | cut -d: -f1)"
+#if [ -z "${XUSER}" ]; then
+#    XUSER="developer"
+#    useradd -u "${XUID}" -g "${XGID}" "developer"
+#fi
